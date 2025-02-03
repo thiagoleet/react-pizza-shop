1b1f485c02a9218ec6c6e66380444568565c695d
Thiago Leite <thiagofmleite@gmail.com>
2025-02-03 11:51:54 -0300

    add Tailwind CSS configuration and utility functions

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import path from "node:path"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 8080,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
