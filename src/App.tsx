c5af178785296d1b2ad71c55ef56ea8aa52553b0
Thiago Leite <thiagofmleite@gmail.com>
2025-02-05 17:57:23 -0300

    feat: add Button component with Radix UI integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/ui/button";
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
