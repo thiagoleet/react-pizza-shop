adb010d4d39935c09a3ff541e9ad18c1117886c5
Thiago Leite <thiagofmleite@gmail.com>
2025-02-06 18:54:24 -0300

    feat: integrate React Query and React Hook Form for store profile dialog

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,16 +1,39 @@
-import { DialogTitle } from "@radix-ui/react-dialog";
+import { useQuery } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import {
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
+  DialogTitle,
 } from "./ui/dialog";
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
+import {
+  storeProfileSchema,
+  StoreProfileSchema,
+} from "@/schemas/store-profile.schema";
 
 export function StoreProfileDialog() {
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ["managed-restaurant"],
+      queryFn: getManagedRestaurant,
+    });
+
+  const { register, handleSubmit } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: managedRestaurant?.name ?? "",
+      description: managedRestaurant?.description ?? "",
+    },
+  });
+
   return (
     <DialogContent>
       <DialogHeader>
@@ -20,7 +43,7 @@ export function StoreProfileDialog() {
         </DialogDescription>
       </DialogHeader>
 
-      <form>
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
         <div className="space-y-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label
@@ -29,7 +52,10 @@ export function StoreProfileDialog() {
             >
               Nome
             </Label>
-            <Input className="col-span-3" />
+            <Input
+              className="col-span-3"
+              {...register("name")}
+            />
           </div>
 
           <div className="grid grid-cols-4 items-center gap-4">
@@ -39,7 +65,10 @@ export function StoreProfileDialog() {
             >
               Descrição
             </Label>
-            <Textarea className="col-span-3" />
+            <Textarea
+              className="col-span-3"
+              {...register("description")}
+            />
           </div>
         </div>
         <DialogFooter>
