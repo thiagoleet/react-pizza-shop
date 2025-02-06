9f5a86a4ea3b42b2181ae963cb1f41286b12b96b
Thiago Leite <thiagofmleite@gmail.com>
2025-02-06 18:09:55 -0300

    feat: add sign-in and sign-up form schemas using Zod for validation

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -6,7 +6,7 @@ import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { SignInForm } from "@/models/sign-in.schema";
+import { SignInForm } from "@/schemas/sign-in.schema";
 import { useMutation } from "@tanstack/react-query";
 import { signIn } from "@/api/sign-in";
 
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -7,7 +7,7 @@ import { useMutation } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { SignUpForm } from "@/models/sign-up.schema";
+import { SignUpForm } from "@/schemas/sign-up.schema";
 import { registerRestaurant } from "@/api/register-restaurant";
 
 export function SignUpPage() {
