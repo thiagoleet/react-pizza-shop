4b8a52f676e19b52d9be2e1dd2950d75e7a94043
Thiago Leite <thiagofmleite@gmail.com>
2025-02-06 08:40:22 -0300

    feat: implement layout components for app and authentication pages

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import { DashboardPage } from "./pages/app/dashboard";
 import { SignInPage } from "./pages/auth/sign-in";
+import { AppLayout } from "./pages/_layouts/app";
+import { AuthLayout } from "./pages/_layouts/auth";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <DashboardPage />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <DashboardPage />,
+      },
+    ],
   },
   {
-    path: "/sign-in",
-    element: <SignInPage />,
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/sign-in",
+        element: <SignInPage />,
+      },
+    ],
   },
 ]);
