f0f3a54b95ff56821adc219b61a71896ba96443e
Thiago Leite <thiagofmleite@gmail.com>
2025-02-06 18:17:00 -0300

    feat: add getManagedRestaurant API function and integrate with account menu

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,8 +8,21 @@ import {
 } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { Building, ChevronDown, LogOut } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 export function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getProfile,
+  });
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ["managed-restaurant"],
+    queryFn: getManagedRestaurant,
+  });
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -17,7 +30,7 @@ export function AccountMenu() {
           variant="outline"
           className="flex items-center gap-2 select-none"
         >
-          Pizza Shop
+          {managedRestaurant?.name ?? "Pizza Shop"}
           <ChevronDown className="w-4 h-4" />
         </Button>
       </DropdownMenuTrigger>
@@ -27,7 +40,7 @@ export function AccountMenu() {
         className="w-56"
       >
         <DropdownMenuLabel className="flex flex-col">
-          <span>John Doe</span>
+          <span>{profile?.name}</span>
           <span className="text-xs font-normal text-muted-foreground">
             john.doe@pizza.shop
           </span>
