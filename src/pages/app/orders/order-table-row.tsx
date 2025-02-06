3a0997eee61c913ce013f51a2e49b87a0f584942
Thiago Leite <thiagofmleite@gmail.com>
2025-02-06 14:55:29 -0300

    feat: add dialog component for order details and integrate with order table

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,18 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableRow, TableCell } from "@/components/ui/table";
 import { Search, ArrowRight, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button
-          variant="outline"
-          size="xs"
-        >
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              variant="outline"
+              size="xs"
+            >
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">123456</TableCell>
       <TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
