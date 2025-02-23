b281e46d9e36a4a5c02bd651e31cbc884edd0e78
Thiago Leite <thiagofmleite@gmail.com>
2025-02-23 16:56:29 -0300

    feat: add Vitest configuration and testing dependencies for enhanced testing capabilities

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import type { UserConfig } from "vite";
+import type { InlineConfig } from "vitest/node";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 8080,
   },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  test: {
+    globals: true,
+  },
+} as UserConfig & {
+  test: InlineConfig;
 });
