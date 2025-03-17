47b7269d8f89883a0fef3939dfab5060189f3b65
Thiago Leite <thiagofmleite@gmail.com>
2025-03-17 10:29:40 -0300

    feat: enhance sign-in mock to use typed request body for improved type safety

--- src/api/mocks/sign-in.mock.ts
+++ src/api/mocks/sign-in.mock.ts
@@ -1,16 +1,21 @@
 import { http, HttpResponse } from "msw";
 
-export const signInMock = http.post("/authenticate", async ({ request }) => {
-  const { email } = await request.json();
+import { SignInBody } from "@/api/sign-in";
 
-  if (email === "john.doe@example.com") {
-    return new HttpResponse(null, {
-      status: 200,
-      headers: {
-        "Set-Cookie": "auth=sample-jwt",
-      },
-    });
+export const signInMock = http.post<never, SignInBody>(
+  "/authenticate",
+  async ({ request }) => {
+    const { email } = await request.json();
+
+    if (email === "john.doe@example.com") {
+      return new HttpResponse(null, {
+        status: 200,
+        headers: {
+          "Set-Cookie": "auth=sample-jwt",
+        },
+      });
+    }
+
+    return new HttpResponse(null, { status: 401 });
   }
-
-  return new HttpResponse(null, { status: 401 });
-});
+);
