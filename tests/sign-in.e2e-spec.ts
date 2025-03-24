1e259d5c2eb725072aae6815fe5d8354abd6fb7d
Thiago Leite <thiagofmleite@gmail.com>
2025-03-24 12:24:37 -0300

    feat: add end-to-end tests for sign-in functionality

---
+++ tests/sign-in.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from "@playwright/test";
+
+test("signin successfully", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByText("Seu e-mail").fill("john.doe@example.com");
+
+  await page.getByRole("button", { name: "Accessar Painel" }).click();
+
+  const toast = page.getByText(
+    "Enviamos um link de autenticação para o seu e-mail"
+  );
+
+  expect(toast).toBeVisible();
+
+  await page.waitForTimeout(2000);
+});
+
+test("signin with wrong credentials", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByText("Seu e-mail").fill("wrong-email@example.com");
+
+  await page.getByRole("button", { name: "Accessar Painel" }).click();
+
+  const toast = page.getByText(
+    "Não foi possível enviar o link de autenticação"
+  );
+
+  expect(toast).toBeVisible();
+
+  await page.waitForTimeout(2000);
+});
+
+test("navigate to new restaurant page", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByRole("link", { name: "Novo Estabelecimento" }).click();
+
+  expect(page.url()).toContain("sign-up");
+});
