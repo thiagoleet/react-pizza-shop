c27f49724b5f63ed08bf24b994d2ef35f798cbce
Thiago Leite <thiagofmleite@gmail.com>
2025-03-24 13:19:08 -0300

    feat: add end-to-end tests for filtering orders by ID, customer name, and status

--- tests/orders.e2e-spec.ts
+++ tests/orders.e2e-spec.ts
@@ -63,3 +63,51 @@ test("paginate orders", async ({ page }) => {
     page.getByRole("cell", { name: "order-10", exact: true })
   ).toBeVisible();
 });
+
+test("filter by order id", async ({ page }) => {
+  await page.goto("/orders", { waitUntil: "networkidle" });
+
+  await page.getByRole("textbox", { name: "ID do Pedido" }).fill("order-66");
+
+  await page.getByRole("button", { name: "Filtrar resulados" }).click();
+
+  await page.waitForLoadState("networkidle");
+
+  expect(
+    page.getByRole("cell", { name: "order-66", exact: true })
+  ).toBeVisible();
+});
+
+test("filter by customer name", async ({ page }) => {
+  await page.goto("/orders", { waitUntil: "networkidle" });
+
+  await page
+    .getByRole("textbox", { name: "Nome do cliente" })
+    .fill("Customer 11");
+
+  await page.getByRole("button", { name: "Filtrar resulados" }).click();
+
+  await page.waitForLoadState("networkidle");
+
+  expect(
+    page.getByRole("cell", { name: "Customer 11 ", exact: true })
+  ).toBeVisible();
+});
+
+test("filter by status", async ({ page }) => {
+  await page.goto("/orders", { waitUntil: "networkidle" });
+
+  await page.getByRole("combobox").click();
+
+  await page.waitForTimeout(1000);
+
+  await page.getByRole("option", { name: "Entregue" }).click();
+
+  await page.getByRole("button", { name: "Filtrar resulados" }).click();
+
+  await page.waitForLoadState("networkidle");
+
+  const tableRows = await page.getByRole("cell", { name: "Entregue" }).all();
+
+  expect(tableRows).toHaveLength(10);
+});
